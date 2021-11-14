07a91c6ba9afec759ebd2568e824dbca7f8658e5
Mirzaev Alisher <Jollyuser7@users.noreply.github.com>
2021-11-15 00:56:22 +0500

    Add new blocks

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,34 @@
 
 //=require ../../../node_modules/jquery/dist/jquery.min.js
 //=require libs/libs.js
+//=require libs/slick.min.js
 
 $(() => {
   //=require main.js
+  $('.partners__list').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,21 +1,2 @@
-const { create } = require("browser-sync");
 
 console.log('main.js');
-
-let topMenu = document.querySelectorAll('.top-menu li a');
-
-for (let i = 0; i < topMenu.length; i++ ) {
-  console.log(topMenu[i].innerHTML)
-}
-
-console.log(topMenu)
-
-// const app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: 'Hello world!'
-//   },
-//   created: function(){
-//     console.log(`Name: ${this.message}`)
-//   }
-// })
