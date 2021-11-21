b84759bc9baf935a7d41bb05e977d8e9715038f3
Mirzaev Alisher <Jollyuser7@users.noreply.github.com>
2021-11-22 01:25:16 +0500

    Finish layout

--- src/views/components/menu/menu.js
+++ src/views/components/menu/menu.js
@@ -7,18 +7,25 @@ $(function() {
       return deistans;
     },
   });
+  // $(document).click( function(e){
+  //   if ( $(e.target).closest('.header__container').length ) {
+  //       return;
+  //   } 
+  //   $('.header__container').removeClass('header__container--active');
+  // });
 });
 
 
 const burgerMenu = document.querySelector('.menu__burger');
-const headerMiddle = document.querySelector('.header__middle');
+const headerMiddle = document.querySelector('.header__container');
 
 if(burgerMenu){
   burgerMenu.addEventListener('click', function(event){
     document.body.classList.toggle('open-menu');
     event.target.classList.toggle('menu__burger--active');
     if(headerMiddle){
-      headerMiddle.classList.toggle('header__middle--active');
+      headerMiddle.classList.toggle('header__container--active');
     }
   })
 }
+
